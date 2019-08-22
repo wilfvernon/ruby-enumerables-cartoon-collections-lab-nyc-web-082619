c10058c18932bb9b580d9b54900ec10d0ddb29ea
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-22 17:29:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  return array.map{|element| element.capitalize + "!"
+  return array.map{|element| element.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
