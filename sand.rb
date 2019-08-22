e6f0bd5c2a3ca61fefebe9ebf347fc7746348b72
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-22 17:38:04 +0000

    Automatically backed up by Learn

--- sand.rb
+++ sand.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves(array)
- array.each {|n| puts n} 
+def long_planeteer_calls(array)
+  if array.detect{|w| w.length > 3}
+    puts true
+    return true
+end
 end
 
-roll_call_dwarves(["Dopey", "Brian", "Winslow"])
+long_planeteer_calls(["cold"])
