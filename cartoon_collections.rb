d0afb58dfa602649c7be49f4bfe53dc4cade425f
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-22 17:31:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(array)
   return array.map{|element| element.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  if array.
 end
 
 def find_the_cheese# code an argument here
