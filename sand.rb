92af79c4a6999b86af34c6cb41c30027e286e8a4
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-22 17:38:50 +0000

    Automatically backed up by Learn

--- sand.rb
+++ sand.rb
@@ -2,8 +2,11 @@ def long_planeteer_calls(array)
   if array.detect{|w| w.length > 3}
     puts true
     return true
+    else 
+      puts false
+      return false
 end
-else return false
+
 end
 
 long_planeteer_calls(["col"])
